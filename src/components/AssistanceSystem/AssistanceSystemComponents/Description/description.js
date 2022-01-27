a3db2682b266e58883a0317129a16194b163ae40
Sandro <sandro.michl@web.de>
2022-01-27 13:58:03 +0100

    feat/setup-content

--- src/components/AssistanceSystem/AssistanceSystemComponents/Description/description.js
+++ src/components/AssistanceSystem/AssistanceSystemComponents/Description/description.js
@@ -3,7 +3,19 @@ import React from 'react'
 import './description.css';
 
 export const Description = () => {
-    return(
-        <p>Dummy Text Description</p>
+    return (
+        <>
+            <p>Hast du an etwas neuem gearbeitet oder etwas ausgebessert?</p>
+            <input type='radio' id='feat' name='featOrFix' value='feat' />
+            <label for='feat'>An etwas neuem</label>
+            <input type='radio' id='fix' name='featOrFix' value='fix' />
+            <label for='fix'>Etwas ausgebessert</label>
+            <p>Definiere bitte einen Überbegriff für das was du gemacht hast</p>
+            <input type="text" id="category" name="name" required size="30" placeholder="z.B.: User Interface, Content, ..." />
+            <p>Beschreibe in 2, 3 oder 4 Worten was genau du gemacht hast.</p>
+            <input type="text" id="description" name="explanation" required size="30" />
+            <p></p>
+            <button>Absenden</button>
+        </>
     )
 }
--- src/components/AssistanceSystem/AssistanceSystemComponents/History/history.js
+++ src/components/AssistanceSystem/AssistanceSystemComponents/History/history.js
@@ -3,7 +3,29 @@ import React from 'react'
 import './history.css'
 
 export const History = () => {
-    return(
-        <p>Dummy Text History</p>
+    return (
+        <>
+            <div>
+                <div>
+                    <p>17.05.2000</p>
+                    <p>feat(userInterface)/implemented-the-interface</p>
+                </div>
+                <img src='/assets/down-arrow.png' alt='arrowUp' />
+            </div>
+            <div>
+                <div>
+                    <p>20.05.2000</p>
+                    <p>feat(content)/implemented-the-content</p>
+                </div>
+                <img src='/assets/down-arrow.png' alt='arrowUp' />
+            </div>
+            <div>
+                <div>
+                    <p>23.05.2000</p>
+                    <p>fix(content)/spelling</p>
+                </div>
+                <img src='/assets/down-arrow.png' alt='arrowUp' />
+            </div>
+        </>
     )
 }
