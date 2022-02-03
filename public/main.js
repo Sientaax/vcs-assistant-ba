0396cdddb3d0605c157b6d12ec56703454ac192d
Sandro <sandro.michl@web.de>
2022-02-03 19:35:47 +0100

    fix/builder

--- public/main.js
+++ public/main.js
@@ -1,17 +1,27 @@
 const { app, BrowserWindow } = require('electron')
 
+const path = require('path')
+const isDev = require('electron-is-dev')
+
 require('@electron/remote/main').initialize()
 
 function createWindow(){
     const win = new BrowserWindow({
         width: 370,
-        height: 700,
+        height: 640,
+        transparent: true,
+        frame: false,
+        skipTaskbar: true,
         webPreferences: {
+            nodeIntegration: true,
             enableRemoteModule: true
         }
     })
 
-    win.loadURL('http://localhost:3000')
+    win.loadURL(
+        isDev ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, '../build/index.html')}`
+    )
 }
 
 app.on('ready', createWindow)
