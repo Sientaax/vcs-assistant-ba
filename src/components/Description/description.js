f9d87cd84f43b159c429f47fa389e5952a891af5
Sandro <sandro.michl@web.de>
2022-02-24 18:32:44 +0100

    fix/tagging bug

--- src/components/Description/description.js
+++ src/components/Description/description.js
@@ -31,7 +31,7 @@ export const Description = ({ closeDescription }) => {
         } else if(category === '' && description !== ''){
             return;
         } else {
-            commitMessage = radio + '(' + category + '/' + description.replace(/ +/g, '-')
+            commitMessage = radio + '(' + category.replace(/ +/g, '-') + '/' + description.replace(/ +/g, '-')
             ws.send(JSON.stringify({
                 id: "commitMessage",
                 data: commitMessage,
